882f6110d8d55f1ad00371d47667c8c4b2d34e05
yuriarodrig <yurialves3324@gmail.com>
2023-05-30 11:14:44 -0300

    Função de consultar o CNPJS

--- app.py
+++ app.py
@@ -13,3 +13,25 @@ password = getenv("PASSWORD")
 
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
+
+def Consult_CNPJ(cnpj):
+    try:
+        site = requests.get(f'https://minhareceita.org/{cnpj}').json()
+        cnpj_id = cnpj
+        
+        simples = site['opcao_pelo_simples']
+        simples_nao = site['data_exclusao_do_simples']
+        
+        if simples:
+            simples = 'Sim'
+        
+        elif simples_nao:
+            simples = 'Não'
+        
+        elif simples == None and simples_nao == None:
+            simples = 'Não'
+            
+        return {'CNPJ':cnpj_id, 'Simples':simples}
+    
+    except:
+        print(f'Erro no cnpj {cnpj}')
