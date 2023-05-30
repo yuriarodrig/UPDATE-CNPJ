b24a983db8f9a7dc993aa54c6674082521ce0c26
yuriarodrig <yurialves3324@gmail.com>
2023-05-30 11:10:53 -0300

    Conect BD

--- app.py
+++ app.py
@@ -5,4 +5,11 @@ import numpy as np
 from os import getenv
 from dotenv import find_dotenv, load_dotenv
 
- 
+load_dotenv(find_dotenv(), override=True)
+server = getenv("SERVER")
+database = getenv("DATABASE")
+username = getenv("USERNAME")
+password = getenv("PASSWORD")
+
+conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = conn.cursor()
